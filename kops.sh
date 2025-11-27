18c667bac9d58db7045a656b5bbd0d5abc044864
shakeerjaykay <shakeerjaykay@gmail.com>
2025-11-27 10:28:43 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -7,6 +7,6 @@ mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 echo "export PATH=$PATH:/usr/local/bin/" >> source .bashrc
 
-export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
-kops create cluster --name rahamss.k8s.local --zones ap-south-1a --control-plane-image ami-02b8269d5e85954ef  --control-plane-count=1 --control-plane-size c7i-flex.large --image ami-02b8269d5e85954ef  --node-count=2 --node-size c7i-flex.large
-kops update cluster --name rahamss.k8s.local --yes --admin
+export KOPS_STATE_STORE=s3://shakeerbucket.k8s.local
+kops create cluster --name shakeer.k8s.local --zones us-east-1 --control-plane-image ami-0fa3fe0fa7920f68e  --control-plane-count=1 --control-plane-size c7i-flex.large --image ami-0fa3fe0fa7920f68e  --node-count=2 --node-size c7i-flex.large
+kops update cluster --name shakeer.k8s.local --yes --admin
